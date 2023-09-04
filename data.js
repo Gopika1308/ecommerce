5ff0f2ea74bd6d16ca66a4d779ba6c907059ad47
GOPIKA M M <128196005+Gopika1308@users.noreply.github.com>
2023-09-04 16:24:13 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,37 @@
+let shopItemsData=[{
+    id:"qwer",
+    name:"Formal Shirt",
+    price:100,
+    desc:"It is a formal wear.",
+    img:"shirt.jpg"
+},{
+    id:"asdf",
+    name:"Tshirt",
+    price:100,
+    desc:"It is a casual wear.",
+    img:"Tshirt.jpg"
+},{
+    id:"zxcv",
+    name:"chain",
+    price:100,
+    desc:"It is a custom chain used for the decoration",
+    img:"chain.jpg"
+},{
+    id:"edfc",
+    name:"watch",
+    price:100,
+    desc:"A branded watch used to show the time",
+    img:"watch.jpg"
+},{
+    id:"abcd",
+    name:"laptop",
+    price:15000,
+    desc:"A branded laptop to carry wherever you go",
+    img:"laptop.jpg"
+},{
+    id:"efgh",
+    name:"shoe",
+    price:1700,
+    desc:"A casual shoe to walk casual",
+    img:"shoe.jpg"
+}];
